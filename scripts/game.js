630936b3ac1313881ffe0618b6682e014f738254
Yask00 <jasstreb@abv.bg>
2017-03-17 17:13:55 +0200

    added second canvas to game.js, todo real animation

--- scripts/game.js
+++ scripts/game.js
@@ -5,20 +5,30 @@ function loadSprite(url) {
 }
 // Define all game sprites here
 const sprites = {
-    guardian: loadSprite('./images/small-guardian.png')
+    guardian: loadSprite('./images/small-guardian.png'),
+    space: loadSprite('./images/space.png')
 };
 
 // Start the game when sprites have loaded
 window.addEventListener('load', function() {
     const canvas = document.getElementById('game-canvas');
+    const spaceCanvas = document.getElementById('space-canvas');
+
     const ctx = canvas.getContext('2d');
+    const spaceCtx = spaceCanvas.getContext('2d');
+
     ctx.clearAll = function() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+    };
+
+    spaceCtx.clearAll = function() {
+        spaceCtx.clearRect(0, 0, spaceCanvas.width, spaceCanvas.height);
     };
 
     // Define game objects (player, enemies...)
     const keyboard = new Keyboard();
     const guardian = new Player(sprites.guardian, canvas);
+    const spaceBackground = new Space(sprites.space, spaceCanvas); 
 
     function gameLoop() {
         // Update
@@ -26,7 +36,11 @@ window.addEventListener('load', function() {
 
         // Draw
         ctx.clearAll();
+        spaceCtx.clearAll();
+        
         guardian.draw(ctx);
+        spaceBackground.draw(spaceCtx);
+        
         requestAnimationFrame(gameLoop);
     }
     gameLoop();
