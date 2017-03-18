460f233a075305c525bb94501f5ce4d90ab3dcce
hankrum <st_kolev@mail.bg>
2017-03-18 15:58:46 +0200

    unit class created to be inherited by all other objects

--- scripts/gameObjects/unit.js
+++ scripts/gameObjects/unit.js
@@ -1,9 +1,41 @@
 class Unit {
-    constructor(x, y, ctx){
+    constructor(x, y, ctx, sprite, speed) {
+        this._ctx = ctx;
+        this._sprite = sprite;
+        this._speed = speed;
+
+        this._x = x;
+        this._y = y;
+    }
+
+    get x() {
+        return this._x;
+    }
+
+    set x(value) {
+        this._x = value;
+    }
+
+    get y() {
+        return this._y;
+    }
+
+    set y(value) {
+        this._y = value;
+    }
+
+    get speed() {
+        return this._speed;
+    }
+
+    set speed(value) {
+        this._speed = value;
+    }
+
+    move() {
 
     }
-    
-    
+
     draw() {
         const ctx = this._ctx;
         ctx.drawImage(
@@ -11,8 +43,5 @@ class Unit {
             this.x, this.y,
             this._sprite.width, this._sprite.height);
 
-        if (this._lasersOn) {
-            this._drawLasers(ctx);
-        }
     }
 }
