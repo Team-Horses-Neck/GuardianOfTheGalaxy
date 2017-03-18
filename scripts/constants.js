69e609fec91cabaa6cdf3fe2e103f4b15a87d0da
Dimitar Pelev <dimitarpelev@gmail.com>
2017-03-18 17:00:45 +0200

    implemented class Shell

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,4 +1,5 @@
 // Global constants
 const PLAYER_SPEED = 7; // px/frame
+const SHELL_SPEED = 16;
 const SPACE_SCROLL_SPEED = 0.3; // px/frame
 const MIN_TIME_BETWEEN_SHOTS = 500; // ms
---
+++ scripts/gameObjects/shell.js
@@ -0,0 +1,27 @@
+class Shell extends Unit{
+    constructor(x, y, ctx, sprite, speed, direction){
+        super(x, y, ctx, sprite, speed);
+
+        this.direction = direction;
+    }
+
+    get direction(){
+        return this._direction;
+    }
+    set direction(value){
+        if(value < 0){
+            this._direction = -1;
+        }
+        else if (value > 0){
+            this._direction = 1;
+        }
+        else{
+            throw new Error("Shell speed can not be 0");
+        }
+    }
+
+    move(){
+        this.y = y + (speed * this.direction);
+    }
+
+}
